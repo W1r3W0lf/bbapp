5992f2ea7be88cd49f016de5a6a50ce1b3b678ff
W1r3W0lf <apple.link.level@gmail.com>
2018-11-07 13:36:11 -0700

    Updated the Baby class to have a name, gender, and redid DueDate

--- app/src/main/java/com/wolf/wire/babyapplication/Baby.java
+++ app/src/main/java/com/wolf/wire/babyapplication/Baby.java
@@ -2,15 +2,47 @@ package com.wolf.wire.babyapplication;
 
 public class Baby {
 
-    public Baby(String Due, int index){
-        String DueDate = Due;
-        int Index_value = index;
-    }
+    String DueDate;
+
+    String Name;
 
     enum gender {
         Male, Female
     }
 
+    gender gender;
 
+
+    public Baby(String Due){
+        DueDate = Due;
+    }
+
+
+    public String getDueDate() {
+        return DueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        DueDate = dueDate;
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+
+    public Baby.gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Baby.gender gender) {
+        this.gender = gender;
+    }
+    
 
 }
--- app/src/main/java/com/wolf/wire/babyapplication/DiaperType.java
+++
@@ -1,5 +0,0 @@
-package com.wolf.wire.babyapplication;
-
-enum DiaperType {
-    poop, pee, both;
-}
