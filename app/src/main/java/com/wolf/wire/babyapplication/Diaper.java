42b6808003ce00a45614fd8bf46edaf2fc6edd9c
pKxL8r <39533856+pKxL8r@users.noreply.github.com>
2018-12-03 13:37:25 -0700

    Added Javadoc comments to the diaper.java class.

--- app/src/main/java/com/wolf/wire/babyapplication/Diaper.java
+++ app/src/main/java/com/wolf/wire/babyapplication/Diaper.java
@@ -24,12 +24,22 @@ public class Diaper {
 
     private diaperType filledWith;
 
+    /**
+     * Takes in a size of the diaper and sets the
+     * size for the Baby it belongs to.
+     * @param size
+     */
     public Diaper(int size){
         full = false;
         this.size = size;
         filledWith = null;
     }
 
+    /**
+     * Will record the date time in which the diaper is full
+     * and record the sixe of the new diaper.
+     * @param size
+     */
     public void change(int size){
         Date date = new Date();
         setDate(date);
@@ -38,38 +48,81 @@ public class Diaper {
         filledWith = null;
     }
 
+    /**
+     * Returns what the diaper was filledwith.
+     * @return
+     */
     public diaperType getFilledWith() { return filledWith; }
 
+    /**
+     * The setter for what the diaper is filledwith
+     * from enum diapertype.
+     * @param filledWith
+     */
     public void setFilledWith(diaperType filledWith) {
         full = true;
         this.filledWith = filledWith;
     }
 
-
+    /**
+     * Getter for the boolean full if the diaper is full
+     * or not.
+     * @return
+     */
     public boolean isFull() {
         return full;
     }
 
+    /**
+     * Sets the boolean full on whether the diaper is
+     * full or not.
+     * @param full
+     */
     public void setFull(boolean full) {
         this.full = full;
     }
 
+    /**
+     * Will be used to set a timer by the user for a
+     * future diaper change to be expected.
+     */
     public void setFuture_timer() {
         future_timer.schedule(task, future_Diaper);
     }
 
+    /**
+     * Takes in the date time object the diaper was
+     * changed and saves it as history.
+     * @param date
+     */
     public void setDate(Date date) {
         history_Diaper = date;
     }
 
+    /**
+     * Getter for the date time object saved in the
+     * history of the diaper changes.
+     * @return
+     */
     public Date getDate() {
         return history_Diaper;
     }
 
+    /**
+     * Will be used to set a future diaper date time for
+     * future diaper timer.
+     * @param future_Diaper
+     */
     public void setFuture_Diaper(Date future_Diaper) {
         this.future_Diaper = future_Diaper;
     }
 
+    /**
+     * This task is what will happen when the timer goes
+     * off for a future diaper timer. Will be included in
+     * stretch to include an alert for user.
+     * @param task
+     */
     public void setTask(TimerTask task) {
         this.task = task;
     }
